3baa1ffc06ed1e44e8a4830428338ee7dcfc3f9d
Phanindra-24 <anusuriphanindra@gmail.com>
2025-12-04 20:14:07 +0530

    Add files via upload

---
+++ FirstLastEqual.java
@@ -0,0 +1,12 @@
+package com.codegnan.arrays;
+
+public class FirstLastEqual {
+public static boolean (int[] nums) {
+	if (nums.length >= 0 && nums[0] == nums[nums.length-1]) {
+		return true;
+	}
+	
+
+	}
+
+}
